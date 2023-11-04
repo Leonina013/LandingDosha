7042bc6410a0f5fa3753fb3be9b15d05cba55340
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-11-04 19:15:54 +0530

    Add files via upload

---
+++ landing.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+# Set page title and icon
+st.set_page_config(page_title="Dosha Prediction App", page_icon="🌿")
+
+# Define the app title and description
+st.title("Discover Your Dosha")
+st.write("Welcome to the Dosha Prediction App, your guide to Ayurvedic wellness!")
+
+# Dosha descriptions
+st.header("What are Vata, Pitta, and Kapha Doshas?")
+st.markdown(
+    "In Ayurveda, the ancient science of holistic healing, it is believed that everyone is a unique combination of three doshas:"
+)
+
+# Display dosha images and descriptions
+st.beta_container()
+with st.beta_columns(3):
+    with st.beta_container():
+        st.image("vata_image.jpg", caption="Vata Dosha", use_container_width=True)
+        st.write("Vata Dosha")
+        st.write(
+            "Vata is associated with the elements of air and ether (space). It represents energy, movement, and change. People with a predominant Vata dosha are creative, quick-thinking, and adaptable.")
+    
+    with st.beta_container():
+        st.image("pitta_image.jpg", caption="Pitta Dosha", use_container_width=True)
+        st.write("Pitta Dosha")
+        st.write(
+            "Pitta is linked to the elements of fire and water. It governs metabolism, digestion, and transformation. People with a predominant Pitta dosha tend to be determined, intelligent, and passionate.")
+    
+    with st.beta_container():
+        st.image("kapha_image.jpg", caption="Kapha Dosha", use_container_width=True)
+        st.write("Kapha Dosha")
+        st.write(
+            "Kapha corresponds to the elements of earth and water. It provides stability and structure, leading to qualities of calmness, patience, and nurturing.")
+st.beta_container()
+
+# CTA (Call to Action)
+st.header("Ready to Discover Your Dosha?")
+st.write("Get started with our Dosha assessment and prediction. Click the button below to begin!")
+
+# Button to start assessment
+if st.button("Start Assessment"):
+    st.write("Redirect to the Dosha assessment page here")
+
+# Disclaimer
+st.markdown("Please note that the Dosha Prediction App is for informational purposes only and should not replace professional advice from an Ayurvedic practitioner.")
+
+# Footer
+st.write("© 2023 Dosha Prediction App. All rights reserved.")
+
